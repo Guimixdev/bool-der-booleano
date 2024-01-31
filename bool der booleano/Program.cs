455a282d7db31c1c9f07f0bc4ee657944143bf7a
GUIW.AV <GUIW.AV@DESKTOP-RKKVASU>
2024-01-31 19:48:41 -0300

    Adicionar arquivos de projeto.

---
+++ bool der booleano/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace teste
+{
+    public class varivel
+    {
+        public static void Main()
+        {
+            int inteiro = 18;
+            double _double = 10.5;
+            string _string = "Eu sou uma string.";
+            char caracter = 'c';
+            bool boolenano = true;
+
+
+            Console.WriteLine("inteiro:" + inteiro);
+            Console.WriteLine("double:" + _double);
+            Console.WriteLine("string:" + _string);
+            Console.WriteLine("caracter:" + caracter);
+            Console.WriteLine("booleano:" + boolenano);
+        }
+    }
+}
+
